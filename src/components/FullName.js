a18eedbcaafda415497f59450269e67e4219f07c
Eddie Madrigal <edmadrigal@yahoo.com>
2020-05-06 16:00:53 -0700

    redux refactored

---
+++ src/components/FullName.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const FullName = () => {
+  return (
+    <p>Full Name Component</p>
+  )
+}
+
+export default FullName;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,27 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { createStore, combineReducers } from 'redux';
+
+function productsReducer(state = [], action) {
+  return state;
+}
+
+function userReducer(state = '', action) {
+  return state;
+}
+
+const allReducers = combineReducers({
+  products: productsReducer,
+  user: userReducer
+});
+
+const store = createStore(allReducers, {
+  products: [{ name: "iPhone"}],
+  user: "Erika"
+});
+
+console.log(store.getState());
 
 ReactDOM.render(
   <React.StrictMode>
