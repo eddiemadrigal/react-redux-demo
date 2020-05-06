72ac0593f7d4cf566d2328505a998e07ba3ed6cc
Eddie Madrigal <edmadrigal@yahoo.com>
2020-05-06 14:14:49 -0700

    build complete

---
+++ build/precache-manifest.63e3ba3880037a4cd398abe05ba21fe6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e3401a268e61842a71a0e385fee9f00",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b84d05445cd94b773862",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "d4ba3516486167bcd126",
+    "url": "/static/js/2.26e0b805.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.26e0b805.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b84d05445cd94b773862",
+    "url": "/static/js/main.6b5bc537.chunk.js"
+  },
+  {
+    "revision": "dcc729586b2f8591d038",
+    "url": "/static/js/runtime-main.6a6f48ea.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
